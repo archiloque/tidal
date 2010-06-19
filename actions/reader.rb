ac07ebfab3b03191c89633c64656f3ba2f1781c8
Julien Kirch <code@archiloque.net>
2010-06-19 21:09:52 +0200

    starting reader display

--- actions/reader.rb
+++ actions/reader.rb
@@ -12,33 +12,33 @@ class Tidal
     feeds_per_category = []
     current_category = -1
     current_count = 0;
-    database['select feeds.id id, feeds.category category, feeds.name name, feeds.display_content display_content, count(posts.id) count from feeds ' +
+    database['select feeds.id id, feeds.category category, feeds.name name, feeds.display_content display_content, feeds.site_uri site_uri, count(posts.id) count from feeds ' +
             'left join posts on feeds.id = posts.feed_id and posts.read = ? group by feeds.id order by feeds.category, feeds.name', false].each do |row|
       if row[:category] != current_category
         if current_category != -1
-          feeds_per_category.last << current_count
+          feeds_per_category.last[:count] = current_count
         end
         current_count = 0
-        feeds_per_category << [row[:category] || '', []]
         current_category = row[:category]
+        feeds_per_category << {:name => (current_category || ''), :feeds => []}
       end
       current_count += row[:count]
-      feeds_per_category.last[1] << {:id => row[:id], :name => row[:name], :count => row[:count], :display_content => row[:display_content]}
+      feeds_per_category.last[:feeds] << {:id => row[:id], :name => row[:name], :count => row[:count], :display_content => row[:display_content], :site_uri => row[:site_uri]}
     end
     halt 200, {'Content-Type' => 'application/json'}, feeds_per_category.to_json
   end
 
-  post '/reader/render/all' do
+  get '/reader/render/all' do
     render_posts ['select posts.id id, posts.published_at published_at, posts.content content, posts.feed_id feed_id ' +
             'from posts, feeds where feeds.id = posts.feed_id and posts.read = ? order by feeds.category, feeds.name', false]
   end
 
-  post '/reader/render/category' do
+  get '/reader/render/category' do
     render_posts ['select posts.id id, posts.published_at published_at, posts.content content, posts.feed_id feed_id ' +
             'from posts, feeds where feeds.id = posts.feed_id and posts.read = ? and feeds.category = ? order by feeds.category, feeds.name', false, params[:name]]
   end
 
-  post '/reader/render/feed' do
+  get '/reader/render/feed/:id' do
     render_posts ['select posts.id id, posts.published_at published_at, posts.content content, posts.feed_id feed_id ' +
             'from posts, feeds where feeds.id = posts.feed_id and posts.read = ? and feeds.id = ? order by feeds.category, feeds.name', false, params[:id]]
   end
@@ -46,12 +46,15 @@ class Tidal
   private
 
   def render_posts query
-        posts_per_feeds = []
+    if params[:displayedIds]
+      Post.filter(:id => params[:displayedIds]).update(:read => true)
+    end
+    posts_per_feeds = []
     current_feed = -1
-    database[*query].each do |row|
+    database[* query].each do |row|
       if row[:feed_id] != current_feed
-        posts_per_feeds << [row[:feed_id]]
         current_feed = row[:feed_id]
+        posts_per_feeds << {:id => current_feed, :posts => []}
       end
       parsed_post = Nokogiri::XML(row[:content])
       title = parsed_post.xpath('/entry/title')[0].content
@@ -60,11 +63,11 @@ class Tidal
       if link
         link = link['href']
       end
-      posts_per_feeds.last << {:published_at => row[:published_at],
-                               :title => title,
-                               :link => link,
-                               :id => row[:id],
-                               :content => content}
+      posts_per_feeds.last[:posts] << {:published_at => row[:published_at].strftime("%d/%m %H:%M"),
+                                       :title => title,
+                                       :link => link,
+                                       :id => row[:id],
+                                       :content => content}
     end
     halt 200, {'Content-Type' => 'application/json'}, posts_per_feeds.to_json
   end
--- public/tidal.js
+++ public/tidal.js
@@ -1,4 +1,5 @@
 var feeds_information = [];
+var displayedIds = [];
 
 $(function () {
     $("#edit_feed_select").change(function() {
@@ -16,26 +17,26 @@ $(function () {
             var result = '<ul><a id="displayAll" href="#" onclick="displayAll(); return false">All</a>';
             $.each(feeds_information, function(i, category) {
 
-                var itemsCount = category[2];
+                var itemsCount = category.count;
                 var isCategoryEmpty = (!itemsCount) || (itemsCount == 0);
-                var categoryName = category[0];
+                var categoryName = category.name;
 
-                result += '<li class="categoryLi" id="category_' + i + '">\n';
-                result += '\t<a id="categoryExpander_' + i + '" class="categoryExpander" onclick="clickExpander(' + i + '); return false" href="#">+</a>\n';
-                result += '\t<a href="#" class="category' + (isCategoryEmpty ? '' : ' categoryWithElements' ) + '" onclick="displayCategory(\'' + categoryName + '\'); return false;">' + categoryName + '</a>';
+                result += '<li class="categoryLi" id="category_' + i + '">\n'
+                        + '\t<a id="categoryExpander_' + i + '" class="categoryExpander" onclick="clickCategoryExpander(' + i + '); return false" href="#">+</a>\n'
+                        + '\t<a href="#" class="categoryInfo' + (isCategoryEmpty ? '' : ' categoryWithElements' ) + '" onclick="displayCategory(\'' + categoryName + '\'); return false;">' + categoryName + '</a>';
                 if (! isCategoryEmpty) {
                     result += ' (' + itemsCount + ')';
                 }
                 result += '\n\t<ul class="feedUL">\n';
-                $.each(category[1], function (i, feed) {
+                $.each(category.feeds, function (i, feed) {
                     var isFeedEmpty = (feed.count == 0);
-                    result += '\t\t<li class="feedLi">~ ';
-                    result += '<a href="#" id="feed_' + feed.id + '" class="feed' + (isFeedEmpty ? '' : ' feedWithElements') + '" onclick="displayFeed(' + feed.id + '); return false;">' + feed.name;
+                    result += '\t\t<li class="feedLi">~ '
+                            + '<a href="#" id="feed_' + feed.id + '" class="feedInfo' + (isFeedEmpty ? '' : ' feedWithElements') + '" onclick="displayFeed(' + feed.id + '); return false;">' + feed.name;
                     if (!isFeedEmpty) {
                         result += ' (' + feed.count + ')';
                     }
-                    result += '</a>';
-                    result += '</li>\n';
+                    result += '</a>'
+                            + '</li>\n';
                 });
                 result += '\t</ul>\n</li>\n';
             });
@@ -45,7 +46,7 @@ $(function () {
     }
 });
 
-function clickExpander(i) {
+function clickCategoryExpander(i) {
     var categoryExpander = $("#categoryExpander_" + i);
     if (categoryExpander.html() == "+") {
         // will expand
@@ -61,13 +62,72 @@ function clickExpander(i) {
 }
 
 function displayAll() {
-    
+    display("/reader/render/all", {});
 }
 
 function displayCategory(name) {
-
+    display("/reader/render/category", {name: name});
 }
 
 function displayFeed(id) {
+    display("/reader/render/feed/" + id, {});
+}
 
+function display(url, params) {
+    params.displayedIds = displayedIds;
+    $.getJSON(url, params, function(data) {
+        var content = $("#readerContent");
+        content.animate({scrollTop:0}, 400, function() {
+            content.slideUp(function() {
+                var result = '';
+                displayedIds = [];
+                $.each(data, function(id, feedItem) {
+                    var feed = getFeed(feedItem.id);
+                    var feedName = feed ? feed.name : '';
+                    result += '\n<div class="feedContent">'
+                            + '\n\t<div class="feedTitle"><a href="' + feed.site_uri + '">' + feedName + '</a></div>';
+                    $.each(feedItem.posts, function(id, post) {
+                        displayedIds.push(post.id);
+                        result += '\n\t\t<div class="postHeader">'
+                                + '<a id="postExpander_' + post.id + '" href="#" onclick="clickPostExpander(' + post.id + '); return false;">' + (feed.display_content ? '-' : '+' ) + '</a> '
+                                + '<a href="' + post.link + '" target="_blank">' + post.title + '</a>'
+                                + '<span class="postDate">' + post.published_at + '</span>'
+                                + '</div>'
+                                + '\n\t\t<div id="post_' + post.id + '" class="postContent' + (feed.display_content ? '' : ' hiddenPost' ) + '">' + post.content + '</div>'
+                    });
+                    result += '\n\t</div>';
+                });
+                content.html(result);
+                content.slideDown();
+            });
+        });
+    });
+}
+
+function clickPostExpander(id) {
+    var postExpander = $("#postExpander_" + id);
+    if (postExpander.html() == "+") {
+        // will expand
+        $("#post_" + id).slideDown(function() {
+            postExpander.html("–");
+        });
+    } else {
+        // will collapse
+        $("#post_" + id).slideUp(function() {
+            postExpander.html('+');
+        });
+    }
+}
+
+// get a feed from its id
+function getFeed(feedId) {
+    var result = null;
+    $.each(feeds_information, function(i, category) {
+        $.each(category.feeds, function (i, feed) {
+            if (feed.id == feedId) {
+                result = feed;
+            }
+        });
+    });
+    return result;
 }
