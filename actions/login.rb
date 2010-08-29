a66f9e6f9fc1836d48dee88bcea126dd7272b117
Julien Kirch <code@archiloque.net>
2010-08-29 13:24:01 +0200

    redirect to the right page after login

--- actions/login.rb
+++ actions/login.rb
@@ -10,7 +10,11 @@ class Tidal
         halt 404, "Error: #{resp.status}"
       end
     elsif ENV['OPENID_URI']
-      headers 'WWW-Authenticate' => Rack::OpenID.build_header(:identifier => ENV['OPENID_URI'])
+      openid_params = {:identifier => ENV['OPENID_URI']}
+      if params[:return_to]
+        openid_params[:return_to] = params[:return_to]
+      end
+      headers 'WWW-Authenticate' => Rack::OpenID.build_header(openid_params)
       halt 401, 'got openid?'
     else
       redirect '/reader'
@@ -28,8 +32,16 @@ class Tidal
   def check_logged
     if (!ENV['OPENID_URI']) || @user_logged
       true
+    elsif resp = request.env['rack.openid.response']
+      if resp.status == :success
+        session[:user] = resp
+        true
+      else
+        halt 404, "Error: #{resp.status}"
+        false
+      end
     else
-      redirect '/login'
+      redirect "/login?return_to=#{CGI::escape(request.url)}"
       false
     end
   end
