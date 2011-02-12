cb568edde2be410bcb84d533d0dfc0b553eb072f
Julien Kirch <code@archiloque.net>
2011-02-12 18:54:42 +0100

    sometimes the content is blank

--- actions/reader.rb
+++ actions/reader.rb
@@ -4,20 +4,20 @@ class Tidal
   get '/reader' do
     if check_logged
       @js_include += ['jquery', 'tidal']
-      @title = 'reader'
+      @title      = 'reader'
       erb :'reader.html'
     end
   end
 
   get '/reader/feeds_info' do
     feeds_per_category = []
-    current_category = -1
-    current_count = 0;
-    database['select feeds.id as id, feeds.category as category, feeds.name as name, feeds.display_content as display_content, feeds.site_uri as site_uri, count(posts.id) as count ' +
-            'from feeds ' +
-            'left join posts on feeds.id = posts.feed_id and posts.read = ? ' +
-            'group by feeds.id, feeds.category, feeds.name, feeds.display_content, feeds.site_uri ' +
-            'order by feeds.category, feeds.name', false].each do |row|
+    current_category   = -1
+    current_count      = 0;
+    database[                                           'select feeds.id as id, feeds.category as category, feeds.name as name, feeds.display_content as display_content, feeds.site_uri as site_uri, count(posts.id) as count ' +
+                                                            'from feeds ' +
+                                                            'left join posts on feeds.id = posts.feed_id and posts.read = ? ' +
+                                                            'group by feeds.id, feeds.category, feeds.name, feeds.display_content, feeds.site_uri ' +
+                                                            'order by feeds.category, feeds.name', false].each do |row|
       if row[:category] != current_category
         if current_category != -1
           feeds_per_category.last[:count] = current_count
@@ -27,11 +27,11 @@ class Tidal
         current_count = 0
       end
       current_count += row[:count]
-      feeds_per_category.last[:feeds] << {:id => row[:id],
-                                          :name => row[:name],
-                                          :count => row[:count],
+      feeds_per_category.last[:feeds] << {:id              => row[:id],
+                                          :name            => row[:name],
+                                          :count           => row[:count],
                                           :display_content => row[:display_content],
-                                          :site_uri => row[:site_uri]}
+                                          :site_uri        => row[:site_uri]}
     end
     if current_category != -1
       feeds_per_category.last[:count] = current_count
@@ -40,23 +40,23 @@ class Tidal
   end
 
   get '/reader/render/all' do
-    render_posts ['select posts.id as id, posts.published_at as published_at, posts.content as content, posts.feed_id as feed_id, feeds.site_uri as site_uri ' +
-            'from posts, feeds where feeds.id = posts.feed_id and posts.read = ? order by feeds.category, feeds.name, posts.published_at', false]
+    render_posts [                                                                                                                                   'select posts.id as id, posts.published_at as published_at, posts.content as content, posts.feed_id as feed_id, feeds.site_uri as site_uri ' +
+                                                                                                                                                         'from posts, feeds where feeds.id = posts.feed_id and posts.read = ? order by feeds.category, feeds.name, posts.published_at', false]
   end
 
   get '/reader/render/category' do
-    render_posts ['select posts.id as id, posts.published_at as published_at, posts.content as content, posts.feed_id as feed_id, feeds.site_uri as site_uri ' +
-            'from posts, feeds where feeds.id = posts.feed_id and posts.read = ? and feeds.category = ? order by feeds.category, feeds.name, posts.published_at', false, params[:name]]
+    render_posts [                                                                                                                                                          'select posts.id as id, posts.published_at as published_at, posts.content as content, posts.feed_id as feed_id, feeds.site_uri as site_uri ' +
+                                                                                                                                                                                'from posts, feeds where feeds.id = posts.feed_id and posts.read = ? and feeds.category = ? order by feeds.category, feeds.name, posts.published_at', false, params[:name]]
   end
 
   get '/reader/render/feed/:id' do
-    render_posts ['select posts.id as id, posts.published_at as published_at, posts.content as content, posts.feed_id as feed_id, feeds.site_uri as site_uri ' +
-            'from posts, feeds where feeds.id = posts.feed_id and posts.read = ? and feeds.id = ? order by feeds.category, feeds.name, posts.published_at', false, params[:id]]
+    render_posts [                                                                                                                                                    'select posts.id as id, posts.published_at as published_at, posts.content as content, posts.feed_id as feed_id, feeds.site_uri as site_uri ' +
+                                                                                                                                                                          'from posts, feeds where feeds.id = posts.feed_id and posts.read = ? and feeds.id = ? order by feeds.category, feeds.name, posts.published_at', false, params[:id]]
   end
 
   get '/reader/render/past/:id' do
-    render_posts ['select posts.id as id, posts.published_at as published_at, posts.content as content, posts.feed_id as feed_id, feeds.site_uri as site_uri ' +
-            'from posts, feeds where feeds.id = posts.feed_id and feeds.id = ? order by feeds.category, feeds.name, posts.published_at limit 50', params[:id]]
+    render_posts [                                                                                                                                          'select posts.id as id, posts.published_at as published_at, posts.content as content, posts.feed_id as feed_id, feeds.site_uri as site_uri ' +
+                                                                                                                                                                'from posts, feeds where feeds.id = posts.feed_id and feeds.id = ? order by feeds.category, feeds.name, posts.published_at limit 50', params[:id]]
   end
 
   get '/reader/postsRead' do
@@ -73,15 +73,18 @@ class Tidal
       Post.filter(:id => params[:displayedIds]).update(:read => true)
     end
     posts_per_feeds = []
-    current_feed = -1
+    current_feed    = -1
     database[* query].each do |row|
       if row[:feed_id] != current_feed
         current_feed = row[:feed_id]
         posts_per_feeds << {:id => current_feed, :posts => []}
       end
       parsed_post = Nokogiri::XML(row[:content])
-      title = parsed_post.xpath('/entry/title')[0].content
-      content = parsed_post.xpath('/entry/summary')[0].andand.content || parsed_post.xpath('/entry/content')[0].andand.content
+      title       = parsed_post.xpath('/entry/title')[0].content
+      content     = parsed_post.xpath('/entry/summary')[0].andand.content
+      if content.blank?
+        content = parsed_post.xpath('/entry/content')[0].andand.content
+      end
       if content
         parsed_content = Nokogiri::HTML(content)
 
@@ -105,10 +108,10 @@ class Tidal
         link = join_and_canonize(row[:site_uri], link['href'])
       end
       posts_per_feeds.last[:posts] << {:published_at => row[:published_at].strftime("%d/%m %H:%M"),
-                                       :title => title,
-                                       :link => link,
-                                       :id => row[:id],
-                                       :content => content}
+                                       :title        => title,
+                                       :link         => link,
+                                       :id           => row[:id],
+                                       :content      => content}
     end
     halt 200, {'Content-Type' => 'application/json'}, posts_per_feeds.to_json
   end
