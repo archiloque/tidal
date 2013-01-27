2ad9541c2a6fb1e48c67a5d63e3d5631af2f50b7
Julien Kirch <code@archiloque.net>
2013-01-27 22:46:24 +0100

    Massive simplification in the reader

--- actions/reader.rb
+++ actions/reader.rb
@@ -5,90 +5,25 @@ class Tidal
     if check_logged
       @js_include += ['jquery', 'tidal']
       @title = 'reader'
+      @posts = Post.
+          join(:feeds, :id => :feed_id).
+          filter('posts.read = ?', false).
+          order(:category, :name, Sequel.desc(:published_at)).
+          select_all(:posts)
+
+      @feeds_per_id = {}
+      Feed.each do |feed|
+        @feeds_per_id[feed.id] = feed
+      end
       erb :'reader.html'
     end
   end
 
-  get '/reader/feeds_info' do
-    feeds_per_category = []
-    current_category = -1
-    current_count = 0
-    database['select feeds.id as id, feeds.category as category, feeds.name as name, feeds.display_content as display_content, feeds.site_uri as site_uri, count(posts.id) as count ' +
-                 'from feeds ' +
-                 'left join posts on feeds.id = posts.feed_id and posts.read = ? ' +
-                 'group by feeds.id, feeds.category, feeds.name, feeds.display_content, feeds.site_uri ' +
-                 'order by feeds.category, feeds.name', false].each do |row|
-      if row[:category] != current_category
-        if current_category != -1
-          feeds_per_category.last[:count] = current_count
-        end
-        current_category = row[:category]
-        feeds_per_category << {:name => (current_category || ''), :feeds => []}
-        current_count = 0
-      end
-      current_count += row[:count]
-      feeds_per_category.last[:feeds] << {:id => row[:id],
-                                          :name => row[:name],
-                                          :count => row[:count],
-                                          :display_content => row[:display_content],
-                                          :site_uri => row[:site_uri]}
-    end
-    if current_category != -1
-      feeds_per_category.last[:count] = current_count
-    end
-    halt 200, {'Content-Type' => 'application/json'}, feeds_per_category.to_json
-  end
-
-  get '/reader/render/all' do
-    render_posts ["#{POST_RENDER_QUERY_START} and posts.read = ? order by feeds.category, feeds.name, posts.published_at", false]
-  end
-
-  get '/reader/render/category' do
-    render_posts ["#{POST_RENDER_QUERY_START} and posts.read = ? and feeds.category = ? order by feeds.category, feeds.name, posts.published_at", false, params[:name]]
-  end
-
-  get '/reader/render/feed/:id' do
-    render_posts ["#{POST_RENDER_QUERY_START} and posts.read = ? and feeds.id = ? order by feeds.category, feeds.name, posts.published_at", false, params[:id]]
-  end
-
-  get '/reader/render/past/:id' do
-    render_posts ["#{POST_RENDER_QUERY_START} and feeds.id = ? order by feeds.category, feeds.name, posts.published_at limit 50", params[:id]]
-  end
-
-  get '/reader/postsRead' do
-    if params[:displayedIds]
-      Post.filter(:id => params[:displayedIds]).update(:read => true)
-    end
-    halt "OK"
-  end
-
-  private
-
-  POST_RENDER_QUERY_START = 'select posts.id as id, posts.published_at as published_at, posts.content as content, posts.title as title, posts.uri as post_uri, posts.feed_id as feed_id, feeds.site_uri as site_uri' +
-      ' from posts, feeds where feeds.id = posts.feed_id'
-
-  def render_posts query
+  post '/reader' do
     unless params[:displayedIds].blank?
-      Post.filter(:id => params[:displayedIds]).update(:read => true)
+      Post.filter(:id => params[:displayedIds].split(',').collect{|i| i.to_i }).update(:read => true)
     end
-    posts_per_feeds = []
-    current_feed = -1
-    database[* query].each do |row|
-      if row[:feed_id] != current_feed
-        current_feed = row[:feed_id]
-        posts_per_feeds << {:id => current_feed, :posts => []}
-      end
-      link = row[:post_uri]
-      if link
-        link = join_and_canonize(row[:site_uri], row[:post_uri])
-      end
-      posts_per_feeds.last[:posts] << {:published_at => row[:published_at].strftime("%d/%m %H:%M"),
-                                       :title => row[:title],
-                                       :link => link,
-                                       :id => row[:id],
-                                       :content => row[:content]}
-    end
-    halt 200, {'Content-Type' => 'application/json'}, posts_per_feeds.to_json
+    redirect '/reader'
   end
 
 end
--- public/tidal.js
+++ public/tidal.js
@@ -2,201 +2,30 @@ var feeds_information = [];
 var displayedIds = [];
 
 $(function () {
-    $("#edit_feed_select").change(function() {
-        var feed = feeds[$(this).val()];
-        if (feed != null) {
-            $("#edit_feed_name").val(feed.name);
-            $("#edit_feed_category").val(feed.category);
-            $("#edit_feed_display_content").attr('checked', feed.display_content);
-            $("#edit_feed_public").attr('checked', feed.public);
-        }
-    });
-    if ($("#readerFeedsInfo").length > 0) {
-        $.get('/reader/feeds_info', function(data) {
-            feeds_information = data;
-            var result = '<ul><a id="displayAll" title="Display all unread items" href="#" onclick="displayAll(); return false">All</a>';
-            $.each(feeds_information, function(i, category) {
-
-                var itemsCount = category.count;
-                var isCategoryEmpty = (!itemsCount) || (itemsCount == 0);
-                var categoryName = category.name;
-
-                result += '<li class="categoryLi" id="categoryLi_' + i + '">\n'
-                        + '\t<a id="categoryExpander_' + i + '" class="categoryExpander" onclick="clickCategoryExpander(' + i + '); return false" href="#">+</a>\n'
-                        + '\t<a href="#" id="category_' + i + '" class="categoryInfo' + (isCategoryEmpty ? '' : ' categoryWithElements' ) + '" title="Display unread items of this category" onclick="displayCategory(\'' + categoryName + '\'); return false;">' + categoryName;
-                if (! isCategoryEmpty) {
-                    result += ' (' + itemsCount + ')';
-                }
-                result += '</a>';
-                result += '\n\t<ul class="feedUL">\n';
-                $.each(category.feeds, function (i, feed) {
-                    feed.category = category.name;
-                    var isFeedEmpty = (feed.count == 0);
-                    result += '\t\t<li class="feedLi">~ '
-                            + '<a href="#" id="feed_' + feed.id + '" class="feedInfo' + (isFeedEmpty ? '' : ' feedWithElements') + '" title="Display unread items of this feed" onclick="displayFeed(' + feed.id + '); return false;">' + feed.name;
-                    if (!isFeedEmpty) {
-                        result += ' (<span id="feedNumberPosts_' + feed.id + '">' + feed.count + '</span>)';
-                    }
-                    result += '</a>'
-                            + '<a title="Display past articles of this feed" onclick="displayPastArticles(' + feed.id + '); return false;" href="#">⇥</a>'
-                            + '</li>\n';
-                });
-                result += '\t</ul>\n</li>\n';
-            });
-            result += '</ul>';
-            $("#readerFeedsInfo").append(result);
-        });
+  $("#edit_feed_select").change(function () {
+    var feed = feeds[$(this).val()];
+    if (feed != null) {
+      $("#edit_feed_name").val(feed.name);
+      $("#edit_feed_category").val(feed.category);
+      $("#edit_feed_display_content").attr('checked', feed.display_content);
+      $("#edit_feed_public").attr('checked', feed.public);
     }
-});
+  });
 
-function clickCategoryExpander(i) {
-    var categoryExpander = $("#categoryExpander_" + i);
-    if (categoryExpander.html() == "+") {
-        // will expand
-        $("#categoryLi_" + i + " > ul").slideDown(function() {
-            categoryExpander.html("–");
-        });
-    } else {
-        // will collapse
-        $("#categoryLi_" + i + " > ul").slideUp(function() {
-            categoryExpander.html('+');
-        });
-    }
-}
-
-function displayAll() {
-    display("/reader/render/all", {}, true, function() {
-        $.each(feeds_information, function(i, category) {
-            // update the categories
-            $("#category_" + i).removeClass("categoryWithElements").html(category.name);
-
-            // update the feeds
-            $.each(category.feeds, function (i, feed) {
-                $("#feed_" + feed.id).removeClass("feedWithElements").html(feed.id.name);
-            });
-        });
-    });
-}
-
-// display unread posts from this category
-function displayCategory(name) {
-    display("/reader/render/category", {name: name}, true, function() {
-        // find the category
-        $.each(feeds_information, function(i, category) {
-            if (category.name == name) {
-
-                // update the category
-                $("#category_" + i).removeClass("categoryWithElements").html(name);
-
-                // update the feeds
-                $.each(category.feeds, function (i, feed) {
-                    $("#feed_" + feed.id).removeClass("feedWithElements").html(feed.name);
-                });
-            }
-        });
-    });
-}
-
-// display unread posts from this feed
-function displayFeed(id) {
-    display("/reader/render/feed/" + id, {}, true, function() {
-        var feed = getFeed(id);
-        $("#feed_" + id).removeClass("feedWithElements").html(feed.name);
-        $.each(feeds_information, function(i, category) {
-            if (category.name == feed.category) {
-
-                // we found the category, now we look if some of its feeds still contain unread messages
-                var numberUnreadMessages = 0;
-                $.each(category.feeds, function (i, f) {
-                    if($("#feed_" + f.id).hasClass("feedWithElements")) {
-                        numberUnreadMessages += parseInt($("#feedNumberPosts_" + f.id).html());
-                    }
-                });
-                if (numberUnreadMessages == 0) {
-                    $("#category_" + i).removeClass("categoryWithElements").html(category.name);
-                } else {
-                    $("#category_" + i).html(category.name + ' (' + numberUnreadMessages + ')');
-                }
-            }
-        });
-    });
-}
-
-function displayPastArticles(id) {
-    display("/reader/render/past/" + id, {}, false, null);
-}
-
-function display(url, params, new_posts, callback) {
-    params.displayedIds = displayedIds;
-    $.getJSON(url, params, function(data) {
-        var content = $("#readerContent");
-        content.slideUp(function() {
-            var result = '';
-            displayedIds = [];
-            $.each(data, function(id, feedItem) {
-                var feed = getFeed(feedItem.id);
-                var feedName = feed ? feed.name : '';
-                result += '\n<div class="feedContent">'
-                        + '\n\t<div class="feedTitle"><a href="' + feed.site_uri + '">' + feedName + '</a></div>';
-                $.each(feedItem.posts, function(id, post) {
-                    if (new_posts) {
-                        displayedIds.push(post.id);
-                    }
-                    result += '\n\t\t<div class="postHeader">'
-                            + '<span class="postExpander"><a id="postExpander_' + post.id + '" href="#" onclick="clickPostExpander(' + post.id + '); return false;">' + (feed.display_content ? '–' : '+' ) + '</a></span> '
-                            + '<a href="' + post.link + '" target="_blank">' + post.title + '</a>'
-                            + '<span class="postDate">' + post.published_at + '</span>'
-                            + '</div>'
-                            + '\n\t\t<div id="post_' + post.id + '" class="postContent' + (feed.display_content ? '' : ' hiddenPost' ) + '">' + post.content + '</div>'
-                });
-                result += '\n\t</div>';
-            });
-            if (result != '') {
-                if (new_posts) {
-                    result += '<div id="readOk"><a href="#" onclick="postsRead(); return false;">I\'ve read it all</a></div>';
-                }
-            } else {
-                result = 'Nothing to display';
-            }
-            content.html(result);
-            content.slideDown();
-            if (callback != null) {
-                callback();
-            }
-        });
-    });
-}
-
-function postsRead() {
-    $.get("/reader/postsRead", {displayedIds: displayedIds}, function(data) {
-        $("#readerContent").slideUp();
-    });
-}
-
-function clickPostExpander(id) {
-    var postExpander = $("#postExpander_" + id);
+  $('.postExpander').click(function () {
+    var postExpander = $(this);
+    var postId = postExpander.attr('id').substr(13)
     if (postExpander.html() == "+") {
-        // will expand
-        $("#post_" + id).slideDown(function() {
-            postExpander.html("–");
-        });
+      // will expand
+      $("#post_" + postId).slideDown(function () {
+        postExpander.html("–");
+      });
     } else {
-        // will collapse
-        $("#post_" + id).slideUp(function() {
-            postExpander.html('+');
-        });
+      // will collapse
+      $("#post_" + postId).slideUp(function () {
+        postExpander.html('+');
+      });
     }
-}
-
-// get a feed from its id
-function getFeed(feedId) {
-    var result = null;
-    $.each(feeds_information, function(i, category) {
-        $.each(category.feeds, function (i, feed) {
-            if (feed.id == feedId) {
-                result = feed;
-            }
-        });
-    });
-    return result;
-}
+    return false;
+  });
+});
